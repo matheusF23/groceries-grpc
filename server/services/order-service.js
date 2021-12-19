f067ac451fc158700a4e5355f29e8d01ad7d3fc9
Matheus Figueiredo <matheus_figueiredo18@hotmail.com>
2021-12-19 18:14:40 -0300

    feat: remove products of order

--- server/services/order-service.js
+++ server/services/order-service.js
@@ -56,7 +56,63 @@ function addProduct(call, callback) {
   callback(null, {})
 }
 
+function removeProduct(call, callback) {
+  const { id, productId, userId, qty } = call.request
+
+  const user = users.find(user => user.id === userId)
+  let product = products.find(product => product.id === productId)
+
+  if (!user) return callback({
+    code: 5,
+    message: "Usuário não encontrado"
+  })
+
+  if (!product) return callback({
+    code: 5,
+    message: "Produto não encontrado"
+  })
+
+  const price = product.price * qty
+  const orderIndex = orders.findIndex(order => order.id === id)
+
+  if (orderIndex === -1) {
+    return callback({
+      code: 5,
+      message: "Pedido não encontrado"
+    })
+  } else {
+    if(orders[orderIndex].user.id !== user.id) return callback({
+      code: 3,
+      message: "O pedido informado pertence a outro usuário"
+    })
+
+    const productIndex = orders[orderIndex].products.findIndex(product => product.product.id === productId)
+    if (productIndex === -1) {
+      return callback({
+        code: 5,
+        message: "Produto não encontrado no pedido"
+      })
+    } else {
+      if (qty > orders[orderIndex].products[productIndex].qty) {
+        return callback({
+          code: 3,
+          message: "Quantidade inválida"
+        })
+      }
+      orders[orderIndex].products[productIndex].qty -= qty
+      if(orders[orderIndex].products[productIndex].qty === 0) {
+        orders[orderIndex].products.splice(productIndex, 1)
+      }
+      orders[orderIndex].totalPrice -= price
+      product.stock += qty
+    }
+  }
+
+  callback(null, {})
+}
+
 module.exports = {
   list,
-  addProduct
+  addProduct,
+  removeProduct
 }

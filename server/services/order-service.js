34cbde57c9353005f6741ebc7cd7678672dd110e
Matheus Figueiredo <matheus_figueiredo18@hotmail.com>
2021-12-19 17:36:31 -0300

    feat: add products in order

--- server/services/order-service.js
+++ server/services/order-service.js
@@ -1,18 +1,62 @@
-const grpc = require('@grpc/grpc-js')
-
 const { orders } = require('../database/orders')
+const { products } = require('../database/products')
+const { users } = require('../database/users')
 
-function list (call, callback) {
+function list(call, callback) {
   const order = orders.find(order => order.id === call.request.id)
 
   if (!order) return callback({
-    code: grpc.status.NOT_FOUND,
+    code: 5,
     message: "Pedido não encontrado"
   })
 
   callback(null, order)
 }
 
+function addProduct(call, callback) {
+  const { id, productId, userId, qty } = call.request
+
+  const user = users.find(user => user.id === userId)
+  let product = products.find(product => product.id === productId)
+
+  if (!user) return callback({
+    code: 5,
+    message: "Usuário não encontrado"
+  })
+
+  if (!product) return callback({
+    code: 5,
+    message: "Produto não encontrado"
+  })
+
+  const price = product.price * qty
+  product.stock -= qty
+  product = { qty, product }
+
+  let order = { id, totalPrice: price, user, products: [product] }
+  const orderIndex = orders.findIndex(order => order.id === id)
+
+  if (orderIndex === -1) {
+    orders.push(order)
+  } else {
+    if(orders[orderIndex].user.id !== user.id) return callback({
+      code: 3,
+      message: "O pedido informado pertence a outro usuário"
+    })
+
+    const productIndex = orders[orderIndex].products.findIndex(product => product.product.id === productId)
+    if (productIndex === -1) {
+      orders[orderIndex].products.push(product)
+    } else {
+      orders[orderIndex].products[productIndex].qty += qty
+    }
+    orders[orderIndex].totalPrice += price
+  }
+
+  callback(null, {})
+}
+
 module.exports = {
-  list
+  list,
+  addProduct
 }
--- server/services/product-service.js
+++ server/services/product-service.js
@@ -1,5 +1,3 @@
-const grpc = require('@grpc/grpc-js')
-
 const { products } = require('../database/products')
 
 function listByCategory(call, callback) {
@@ -7,7 +5,7 @@ function listByCategory(call, callback) {
   const productsByCategory = products.filter(product => product.category.id === category.id)
 
   if (productsByCategory.length === 0) return callback({
-    code: grpc.status.NOT_FOUND,
+    code: 5,
     message: "Produtos não encontrados"
   })
 

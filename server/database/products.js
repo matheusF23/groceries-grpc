c9bf6147193d6c38afe29a2ab0540755477506d7
Matheus Figueiredo <matheus_figueiredo18@hotmail.com>
2021-12-19 12:04:20 -0300

    feat: add list products by category

--- server/database/products.js
+++ server/database/products.js
@@ -5,70 +5,70 @@ module.exports = {
       'description': 'Banana (unidade)',
       'stock': 50,
       'price': 0.5,
-      'category': 'Frutas'
+      'category': { id: 1, description: 'Frutas' }
     },
     {
       'id': 2,
       'description': 'Maçã (unidade)',
       'stock': 50,
       'price': 1,
-      'category': 'Frutas'
+      'category': { id: 1, description: 'Frutas' }
     },
     {
       'id': 3,
       'description': 'Coca Cola (L)',
       'stock': 20,
       'price': 5,
-      'category': 'Bebidas'
+      'category': { id: 2, description: 'Bebidas' }
     },
     {
       'id': 4,
       'description': 'Leite (L)',
       'stock': 100,
       'price': 3,
-      'category': 'Bebidas'
+      'category': { id: 2, description: 'Bebidas' }
     },
     {
       'id': 5,
       'description': 'Agua (L)',
       'stock': 200,
       'price': 2,
-      'category': 'Bebidas'
+      'category': { id: 2, description: 'Bebidas' }
     },
     {
       'id': 6,
       'description': 'Sal (kg)',
       'stock': 90,
       'price': 1,
-      'category': 'Perecíveis'
+      'category': { id: 3, description: 'Perecíveis' }
     },
     {
       'id': 7,
       'description': 'Açucar (kg)',
       'stock': 40,
       'price': 3,
-      'category': 'Perecíveis'
+      'category': { id: 3, description: 'Perecíveis' }
     },
     {
       'id': 8,
       'description': 'Macarrão',
       'stock': 78,
       'price': 6,
-      'category': 'Perecíveis'
+      'category': { id: 3, description: 'Perecíveis' }
     },
     {
       'id': 9,
       'description': 'Bisteca (kg)',
       'stock': 20,
       'price': 40,
-      'category': 'Frios'
+      'category': { id: 4, description: 'Frios' }
     },
     {
       'id': 10,
       'description': 'Frango (kg)',
       'stock': 30,
       'price': 21,
-      'category': 'Frios'
+      'category': { id: 4, description: 'Frios' }
     }
   ]
 }
--- server/services/product-service.js
+++ server/services/product-service.js
@@ -1,5 +1,11 @@
 const { products } = require('../database/products')
 
 module.exports = {
-  List: (_, callback) => callback(null, { products })
+  List: (_, callback) => callback(null, { products }),
+  ListByCategory: (call, callback) => {
+    const category = call.request
+    const productsByCategory = products.filter(product => product.category.id === category.id)
+
+    callback(null, { products: productsByCategory})
+  }
 }
